aa6296196637b9cc85f7f68d75a2ab4c44ef8a4a
wangxiaopeng <wangxiaopeng@yiwang.com>
2015-01-22 12:26:14 +0800

    cart

---
+++ src/main/java/com/afd/web/controller/CartController.java
@@ -0,0 +1,241 @@
+package com.afd.web.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.afd.common.util.CartTransferUtils;
+import com.afd.constants.order.OrderConstants;
+import com.afd.param.cart.Cart;
+import com.afd.param.cart.CartItem;
+import com.afd.service.order.ICartService;
+import com.afd.service.product.IProductService;
+import com.afd.web.service.impl.LoginServiceImpl;
+import com.afd.param.cart.CookieCartItem;
+import com.afd.common.util.RequestUtils;
+
+@Controller
+@RequestMapping("/cart")
+public class CartController{
+	private static final Logger log = LoggerFactory
+			.getLogger(CartController.class);
+
+	@Autowired
+	private IProductService productService;
+	@Autowired
+	private ICartService cartService;
+
+	private static final String cookie = "[{\"num\":2,\"selected\":false,\"skuId\":37},{\"num\":2,\"selected\":false,\"skuId\":29},{\"num\":2,\"selected\":false,\"skuId\":1},{\"num\":2,\"selected\":false,\"skuId\":40}]";
+
+	/**
+	 * 修改购物车商品数量
+	 * 
+	 * @param cookieCart
+	 * @param skuId
+	 * @param newQuantity
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("/modify")
+	public CartItem modifyQuantity(
+			@CookieValue(value = "cart", required = false) String cookieCart,
+			long bsDetailId, long newQuantity, long oldQuantity,
+			HttpServletRequest request, HttpServletResponse response) {
+		// 修改商品数量
+		List<CartItem> cartItems = this.cartService.modifyQuantity(cookieCart, bsDetailId,
+				newQuantity, oldQuantity);
+		
+		saveCart(
+				CartTransferUtils.cartItemsToCookieCartItems(cartItems),
+				request, response);
+		CartItem cartItem = new CartItem();
+		if (cartItems != null && cartItems.size() > 0) {
+			for (CartItem ci : cartItems) {
+				if (ci.getBrandShowDetailId().equals(bsDetailId)) {
+					cartItem = ci;
+				}
+			}
+		}
+
+		return cartItem;
+	}
+
+	/**
+	 * 展示购物车
+	 * 
+	 * @param cookieCart
+	 * @param modelMap
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping("/cartContent")
+	public String showCart(
+			@CookieValue(value = "cart", required = false) String cookieCart,
+			ModelMap modelMap, HttpServletResponse response,
+			HttpServletRequest request) {
+		// 展示购物车
+		List<Cart> carts = cartService.showCart(cookieCart);
+		// 保存购物车
+		saveCart(CartTransferUtils.cartToCookieCartItems(carts), request,
+				response);
+		// 将carts添加到模型中
+		modelMap.addAttribute("carts", carts);
+		boolean isEmpty = Boolean.parseBoolean(request.getParameter("isEmpty"));
+		boolean hasError = Boolean.parseBoolean(request
+				.getParameter("hasError"));
+		// 是否没有选商品结算（用户点击结算按钮）
+		modelMap.addAttribute("isEmpty", isEmpty);
+		// 结算商品是否有错误（点击结算按钮）
+		modelMap.addAttribute("hasError", hasError);
+		return "cartContent";
+	}
+
+	/**
+	 * 删除商品
+	 * 
+	 * @param cookieCart
+	 * @param skuId
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping("/delete")
+	public String delete(
+			@CookieValue(value = "cart", required = false) String cookieCart,
+			HttpServletRequest request, Long bsDetailId, HttpServletResponse response) {
+		if (bsDetailId != null && bsDetailId > 0) {
+			Set<Long> bsDetailIds = new HashSet<Long>();
+			bsDetailIds.add(bsDetailId);
+			// 删除商品
+			List<CartItem> cartItems = this.cartService.deleteCartItems(cookieCart, bsDetailIds);
+			saveCart(
+					CartTransferUtils.cartItemsToCookieCartItems(cartItems),
+					request, response);
+		}
+		// 跳回购物车
+		return "redirect:/cart/cartContent.action";
+	}
+
+	/**
+	 * 删除选中商品
+	 * 
+	 * @param cookieCart
+	 * @param skuIds
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping("/deleteSelected")
+	public String deleteSelected(
+			@CookieValue(value = "cart", required = false) String cookieCart,
+			HttpServletRequest request, @RequestParam("bsDetailIds") String bsDetailIds,
+			HttpServletResponse response) {
+		if (StringUtils.isNotBlank(bsDetailIds)) {
+			Set<Long> setBsDetailIds = new HashSet<Long>();
+			for (String bsDetailId : bsDetailIds.split(",")) {
+				setBsDetailIds.add(Long.parseLong(bsDetailId));
+			}
+			// 批量删除商品
+			List<CartItem> cartItems = this.cartService.deleteCartItems(cookieCart, setBsDetailIds);
+			saveCart(
+					CartTransferUtils.cartItemsToCookieCartItems(cartItems),
+					request, response);
+		}
+		// 跳回购物车
+		return "redirect:/cart/cartContent.action";
+	}
+
+	/**
+	 * 清除失效商品
+	 * 
+	 * @param cookieCart
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping("/clearFailure")
+	public String clearFailure(
+			@CookieValue(value = "cart", required = false) String cookieCart,
+			HttpServletRequest request, HttpServletResponse response) {
+		// 清除失效商品
+		List<CartItem> cartItems = this.cartService.clearFailureProduct(cookieCart);
+		saveCart(CartTransferUtils.cartItemsToCookieCartItems(cartItems),
+				request, response);
+		// 跳回购物车
+		return "redirect:/cart/cartContent.action";
+	}
+
+	/**
+	 * 验证结算商品(用于异步调用，返回json)
+	 * 
+	 * @param cookieCart
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("/validCartConfirm")
+	public String validCartConfirm(HttpServletRequest request,
+			HttpServletResponse response,
+			@CookieValue(value = "cart", required = false) String cookieCart) {
+		// 将cookie转化成carts
+		List<CartItem> cartItems = this.cartService.getCartItemsByCookie(cookieCart);
+		// 选中的商品组合成购物车
+		cartItems = this.cartService.filterSelectedCart(cartItems);
+		Map<String, Boolean> rtnMap = new HashMap<String, Boolean>();
+		// 设置结算商品是否为空
+		if (cartItems.isEmpty()) {
+			rtnMap.put("isEmpty", true);
+		} else {
+			rtnMap.put("isEmpty", false);
+		}
+		// 设置结算商品是否有误
+		if (this.cartService.hasErrorOnSelectedCart(cartItems)) {
+			rtnMap.put("hasError", true);
+		} else {
+			rtnMap.put("hasError", false);
+		}
+		// 返回json
+		return JSON.toJSONString(rtnMap);
+	}
+
+	@RequestMapping("/chgChecked")
+	public void chgChecked(
+			@CookieValue(value = "cart", required = false) String cookieCart,
+			@RequestParam String bsDetails, @RequestParam boolean checked,
+			HttpServletRequest request, HttpServletResponse response) {
+		Set<Long> setBsDetailsIds = new HashSet<Long>();
+		for (String bsDetail : bsDetails.split(",")) {
+			setBsDetailsIds.add(Long.parseLong(bsDetail));
+		}
+		List<CartItem> cartItems = this.cartService.chgChecked(cookieCart, setBsDetailsIds, checked);
+		saveCart(CartTransferUtils.cartItemsToCookieCartItems(cartItems),
+				request, response);
+	}
+	
+	private void saveCart(List<CookieCartItem> combineCartItems,
+			HttpServletRequest request,
+			HttpServletResponse response
+			){
+		if(combineCartItems == null){
+			combineCartItems = new ArrayList<CookieCartItem>();
+		}
+		RequestUtils.setCookie(request, response, OrderConstants.COOKIE_CART, 
+					JSON.toJSONString(combineCartItems), OrderConstants.COOKIE_CART_PERIOD);
+	}
+
+}
---
+++ src/main/java/com/afd/web/controller/TradeController.java
@@ -0,0 +1,440 @@
+package com.afd.web.controller;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.afd.constants.order.OrderConstants;
+import com.afd.model.order.Order;
+import com.afd.model.user.Geo;
+import com.afd.model.user.User;
+import com.afd.model.user.UserAddress;
+import com.afd.param.cart.Cart;
+import com.afd.param.cart.CartItem;
+import com.afd.param.cart.CookieCartItem;
+import com.afd.param.cart.Trade;
+import com.afd.param.cart.TradeItem;
+import com.afd.param.cart.TradesInfo;
+import com.afd.param.order.OrderInfo;
+import com.afd.service.order.ICartService;
+import com.afd.service.order.IOrderService;
+import com.afd.service.payment.IPaymentServices;
+import com.afd.service.product.IProductService;
+import com.afd.service.user.IAddressService;
+import com.afd.service.user.IUserService;
+import com.afd.common.util.CartTransferUtils;
+import com.afd.common.util.RequestUtils;
+import com.afd.web.service.impl.LoginServiceImpl;
+
+/**
+ * @author xiaotao
+ * 
+ */
+@Controller
+public class TradeController{
+
+	@Autowired
+	private IAddressService addressService;
+	@Autowired
+	private ICartService cartService;
+	@Autowired
+	private IProductService productService;
+	@Autowired
+	private IUserService userService;
+	@Autowired
+	private IOrderService orderService;
+	@Autowired
+	private IPaymentServices paymentServices;
+
+	private static final String USER_NO_LOGIN_TEL_VALIDATOR_PREFIX = "USER_NO_LOGIN_TEL_VALIDATOR_";
+
+	private static final Logger log = LoggerFactory
+			.getLogger(TradeController.class);
+
+	@RequestMapping(value = "/trade")
+	public String tradeinfo(
+			@CookieValue(value = "cart", required = true, defaultValue = "") String cookieCart,
+			Model model, HttpServletRequest request, HttpServletResponse response) {
+		boolean isLogin = LoginServiceImpl.isLogin(request, response);
+		long userId = Long.parseLong(LoginServiceImpl.getUserIdByCookie(request));
+		List<Cart> carts_confirm=new ArrayList<Cart>();
+		carts_confirm = this.cartService.showCart(cookieCart);
+		//保存购物车
+		this.saveCart(CartTransferUtils.cartToCookieCartItems(carts_confirm), request, response);
+		List<Cart> selectedCarts = this.getGoods(carts_confirm);
+		if (selectedCarts == null || selectedCarts.size() == 0) {
+			return "redirect:/cart.jsp";
+		}
+		
+		List<CookieCartItem> cartItems = CartTransferUtils.cartToCookieCartItems(carts_confirm);
+		
+		RequestUtils.setCookie(request, response, OrderConstants.COOKIE_CART + "confirm", 
+				JSON.toJSONString(cartItems), OrderConstants.COOKIE_CART_PERIOD);
+		
+		if (userId != 0l) {
+			return "tradelogin_n";
+		} else {			
+			return "trade_n";
+		}
+	}
+
+	/**
+	 * create order
+	 */
+	@RequestMapping(value = "/tradecomfirm")
+	public String tradeConfirm(TradesInfo tradesInfo, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+		boolean isLogin = LoginServiceImpl.isLogin(request, response);
+		Long uid = Long.parseLong(LoginServiceImpl.getUserIdByCookie(request));
+		if(!isLogin) {
+			//TODO
+			return "";
+		}
+		String submitcookie = RequestUtils.getCookieValue(request, "tradesubmit");
+		if(StringUtils.isNumeric(submitcookie) && NumberUtils.toInt(submitcookie)>1){
+			return "redirect:/cart.jsp";
+		}
+		String ip = request.getRemoteAddr();//返回发出请求的IP地址
+		String cookieCart_confirm = RequestUtils.getCookieValue(request, OrderConstants.COOKIE_CART + "confirm");// 确认购买的商品
+		if (StringUtils.isEmpty(cookieCart_confirm)) {//no cart_confirm cookie
+			log.error("---------------create oreder info:cookie_null------------");
+			cookieCart_confirm = "";
+			return "tradefail";
+		}
+		
+		List<Cart> carts_confirm = this.cartService.showCart(cookieCart_confirm);
+		
+		if (carts_confirm == null || carts_confirm.size() == 0) {
+			return "tradefail";
+		}
+		List<CartItem> cartItems = this.getNoChoosedCartItem(carts_confirm);
+		carts_confirm = this.getGoods(carts_confirm);
+		UserAddress address = this.addressService.getAddressById(tradesInfo.getAddressId());		
+		if(null == address){
+			log.error("---------------address is null------------addressId:"+tradesInfo.getAddressId());
+			return "tradefail";
+		}
+
+		Iterator<Cart> it = carts_confirm.iterator();
+		boolean hasError = false;
+		while (it.hasNext()) {
+			Cart cart = it.next();
+			if (this.cartService.hasErrorOnSelectedCart(cart.getCartItems())) {
+				hasError = true;
+				break;
+			}
+		}
+		if (hasError) {
+			log.error("---------------create oreder info:hasError------------");
+			return "tradefail";// to do cart page
+		}
+		List<Trade> tradelist = this.getTradeInfo(carts_confirm, tradesInfo, uid);
+		if (tradelist == null) {
+			log.error("---------------create oreder info:tradelist==null------------");
+			return "tradefail";// 参数错误
+		}
+		StringBuffer orderids = new StringBuffer();
+		String orderids_str = "";
+		try {
+			List<OrderInfo> orderlist = this.orderService.batchSaveOrders(tradelist);
+			if (orderlist == null || orderlist.size() < 1) {
+				log.error("---------------orderlist == null------------");
+				return "tradefail";
+			}
+			List<CookieCartItem> combimeCartItems = CartTransferUtils
+					.cartItemsToCookieCartItems(cartItems);
+			
+			this.saveCart(combimeCartItems, request, response);
+			
+			log.error("---------------create oreder info0 "
+					+ JSON.toJSONString(cartItems) + "------------");
+			log.error("---------------create oreder info1 "
+					+ JSON.toJSONString(combimeCartItems) + "------------");
+			log.error("---------------create oreder info2 "
+					+ JSON.toJSONString(orderlist) + "------------");
+			
+			for (OrderInfo in : orderlist) {
+				log.error("success:"+in.getCode());
+				if(in.getCode() > 0){
+					orderids.append(in.getOrder().getOrderId()).append("_");
+				}
+			}
+			
+			log.error("orderids:"+orderids);
+			if("".equals(orderids.toString())){
+				log.error("---------------create oreder orderids_str empty-------- ");
+				return "tradefail";
+			}
+			
+			orderids_str = orderids.substring(0, orderids.length() - 1);
+			RequestUtils.deleteCookie(request, response, OrderConstants.COOKIE_CART + "confirm", true);
+		} catch (Exception e) {
+			log.error("---------------create oreder error"
+					+ JSON.toJSONString(tradelist) + "------------");
+			log.error(e.getMessage(),e);
+			return "tradefail";
+		}
+		
+		String[] ids = orderids_str.split("_");			
+		List<Long> orderids_list =new ArrayList<Long>();
+		
+		for (int i = 0; i < ids.length; i++) {				
+			orderids_list.add(new Long(ids[i]));
+		}			
+		String paymentType="1";
+		if(orderids_list.size() > 1){
+			paymentType="2";
+		}
+		String paymode=tradesInfo.getPayMode();
+		if(tradesInfo.getPayMode().substring(0, 1).equals("1")){
+			paymode=OrderConstants.PAY_MODE_APIPAY;
+		}
+		if(tradesInfo.getPayMode().substring(0, 1).equals("2")){
+			paymode=OrderConstants.PAY_MODE_CHINAPAY;
+		}
+		Long paymentId=this.paymentServices.getPaymentId(orderids_list, "1", ip, uid, paymode, paymentType);
+		if(null==paymentId||paymentId<0l){
+			return "payfail";
+		}
+		return "redirect:/payment.action?uid=" + uid + "&paymentid="
+				+ paymentId;
+	}
+
+	@RequestMapping(value = "/tradesucceed")
+	public String tradesucceed(HttpServletRequest request, ModelMap modelMap,
+			HttpServletResponse response) {
+		String orderids = request.getParameter("orderids");
+		Long userId = Long.parseLong(LoginServiceImpl.getUserIdByCookie(request));
+		if (StringUtils.isBlank(orderids)||userId.equals(0l)) {
+			return "redirect:/cart.jsp";
+		}
+		String[] ids = orderids.split("_");
+		Long[] ids_list;
+		ids_list = new Long[ids.length];
+		for (int i = 0; i < ids.length; i++) {
+			ids_list[i] = (new Long(ids[i]));
+		}
+		//TODO
+//		List<Order> orderlist = this.orderService.getOrdersByIdsAndUserIdNodelay(ids_list, userId);
+		List<Order> orderlist = new ArrayList<Order>();
+		modelMap.addAttribute("orderlist", orderlist);
+		return "tradesucceed_n";
+	}
+
+	/**
+	 * area operations
+	 */
+	@RequestMapping(method = RequestMethod.GET, value = "/geo")
+	@ResponseBody
+	public ResponseEntity<?> viewGeo(@RequestParam(value = "fid") Long fid) {
+		//TODO
+//		List<Geo> geoList = this.addressService.getGeoByFid(fid);
+		List<Geo> geoList = new ArrayList<Geo>();
+		return new ResponseEntity<List<Geo>>(geoList, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/tradeGoods")
+	public String goods(@CookieValue(value = "cartconfirm", required = true, defaultValue = "") String cookieCart,
+			ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+		String addressId = request.getParameter("addressId");
+		
+		if(StringUtils.isNotBlank(addressId)){
+			UserAddress address = this.addressService.getAddressById(Integer.parseInt(addressId));
+			if(address!=null){
+				modelMap.addAttribute("addr", address);
+			}
+		}
+		List<Cart> carts = this.cartService.showCart(cookieCart);
+		
+		if(this.isCartEmpty(carts)){
+			modelMap.addAttribute("carts", carts);
+		}
+		List<Cart> selectedCarts = this.getGoods(carts);
+
+		modelMap.addAttribute("carts", selectedCarts);
+
+		return "tradeGoods";
+
+	}
+
+	/**
+	 * address operations
+	 */
+	@RequestMapping(value = "/addr")
+	@ResponseBody
+	public ResponseEntity<?> getAddrinfo(HttpServletRequest request, HttpServletResponse response) {
+		Long uid = Long.parseLong(LoginServiceImpl.getUserIdByCookie(request));
+		if (uid != 0l) {
+			List<UserAddress> addrs = this.addressService.getAddressesByUserId(uid);
+			return new ResponseEntity<List<UserAddress>>(addrs, HttpStatus.OK);
+		}
+		return new ResponseEntity<Boolean>(false, HttpStatus.OK);
+	}
+
+	/**
+	 * get TradeInfo
+	 * 
+	 * @param cats
+	 * @return TradeInfo
+	 */
+	private List<Trade> getTradeInfo(List<Cart> cats, TradesInfo tradesInfo, Long userId) {
+		List<Trade> trades = new ArrayList<Trade>();
+		Iterator<Cart> it = cats.iterator();
+		User user = this.userService.getUserById(userId);
+		while (it.hasNext()) {
+			Cart cartTemp = it.next();
+			List<TradeItem> tradeItems = this.getTradeItem(
+					cartTemp.getCartItems(), cartTemp.getSellerId());
+			if (tradeItems.size() == 0) {
+				continue;
+			}
+			Trade trade = new Trade();
+			trade.setUserId(userId);
+			trade.setSellerId(cartTemp.getSellerId());
+			trade.setTradeItems(tradeItems);
+			trade.setAddressId(tradesInfo.getAddressId());
+			
+			trade.setPayType("1");
+			trade.setPayMode(tradesInfo.getPayMode());
+
+			trade.setOrderType("1");
+			trade.setOrderSource("1");
+			trade.setDeliverFee(new BigDecimal("0"));
+
+			trade.setDeliverDiscountFee(new BigDecimal("0"));
+			trade.setOptName(user.getUserName());
+
+			trades.add(trade);
+		}
+
+		return trades;
+	}
+
+	/**
+	 * get TradeItem Info
+	 * 
+	 * @param cartItems
+	 * @return TradeItem Info
+	 */
+	private List<TradeItem> getTradeItem(List<CartItem> cartItems, Long sellerId) {
+		List<TradeItem> tradeItems = new ArrayList<TradeItem>();
+		Iterator<CartItem> it = cartItems.iterator();
+
+		while (it.hasNext()) {
+			CartItem cartItemTemp = it.next();
+			if (cartItemTemp.isSelected()) {
+				TradeItem tradeItem = new TradeItem();
+				tradeItem.setProdId(cartItemTemp.getProdId());
+				tradeItem.setNum(cartItemTemp.getNum());
+				tradeItem.setMarketPrice(cartItemTemp.getMaketPrice());
+				tradeItem.setShowPrice(cartItemTemp.getShowPrice());
+				tradeItem.setSellerId(sellerId);
+				tradeItem.setSkuId(cartItemTemp.getSkuId());
+				tradeItem.setProdSpecId(cartItemTemp.getSpecId());
+				tradeItem.setProdSpecName(cartItemTemp.getSpecName());
+				tradeItem.setProdTitle(cartItemTemp.getProdName());
+				tradeItem.setBcId(cartItemTemp.getBcId().longValue());
+				tradeItem.setProdCode(cartItemTemp.getProdCode());
+				tradeItem.setSkuCode(cartItemTemp.getSkuCode());
+
+				tradeItems.add(tradeItem);
+			}
+		}
+		return tradeItems;
+	}
+
+	
+	private boolean isCartEmpty(List<Cart> carts) {
+		boolean ret = true;
+		Iterator<Cart> it = carts.iterator();
+		while (it.hasNext()) {
+			Cart cart = it.next();
+			for (int j = 0; j < cart.getCartItems().size(); j++) {
+				CartItem cartItem = cart.getCartItems().get(j);
+				if (cartItem.isSelected()) {	
+					return false;
+				}
+			}			
+		}
+		return ret;
+	}
+	
+	/*
+	 * 购物车中选中的商品
+	 * 
+	 * @param carts
+	 * 
+	 * @param ids
+	 * 
+	 * @param response
+	 */
+	private List<Cart> getGoods(List<Cart> carts) {
+		carts = new ArrayList<Cart>(carts);
+		Set<Cart> cleanShopids = new HashSet<Cart>();
+		Set<CartItem> cleanIds = new HashSet<CartItem>();
+		Iterator<Cart> it = carts.iterator();
+		while (it.hasNext()) {
+			Cart cart = it.next();
+			for (int j = 0; j < cart.getCartItems().size(); j++) {
+				CartItem cartItem = cart.getCartItems().get(j);
+				if (!cartItem.isSelected()) {
+					cleanIds.add(cartItem);
+				}
+			}
+			cart.getCartItems().removeAll(cleanIds);
+			if (cart.getCartItems().size() == 0) {
+				cleanShopids.add(cart);
+			}
+		}
+		carts.removeAll(cleanShopids);
+		return carts;
+	}
+
+	private List<CartItem> getNoChoosedCartItem(List<Cart> carts) {
+		List<CartItem> cartItems = new ArrayList<CartItem>();
+		Iterator<Cart> it = carts.iterator();
+		while (it.hasNext()) {
+			Cart cart = it.next();
+			for (int j = 0; j < cart.getCartItems().size(); j++) {
+				CartItem cartItem = cart.getCartItems().get(j);
+				if (!cartItem.isSelected()) {
+					cartItems.add(cartItem);
+				}
+			}
+		}
+		return cartItems;
+	}
+	
+	private void saveCart(List<CookieCartItem> combineCartItems,
+			HttpServletRequest request,
+			HttpServletResponse response
+			){
+		if(combineCartItems == null){
+			combineCartItems = new ArrayList<CookieCartItem>();
+		}
+		RequestUtils.setCookie(request, response, OrderConstants.COOKIE_CART, 
+					JSON.toJSONString(combineCartItems), OrderConstants.COOKIE_CART_PERIOD);
+	}
+}
